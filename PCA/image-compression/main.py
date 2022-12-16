186c3567c9f237f8fe3559048c3cb4ec8e7d4690
sendo_mac <vietpa@sendo.vn>
2022-12-16 17:24:32 +0700

    WIP

---
+++ PCA/image-compression/main.py
@@ -0,0 +1,18 @@
+"""
+https://towardsdatascience.com/dimensionality-reduction-of-a-color-photo-splitting-into-rgb-channels-using-pca-algorithm-in-python-ba01580a1118
+https://towardsdatascience.com/pca-clearly-explained-how-when-why-to-use-it-and-feature-importance-a-guide-in-python-7c274582c37e
+https://towardsdatascience.com/feature-extraction-using-principal-component-analysis-a-simplified-visual-demo-e5592ced100a
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+import cv2
+from scipy.stats import stats
+import matplotlib.image as mpimg
+
+if __name__ == "__main__":
+    img = cv2
+    plt.imshow(img)
+    plt.show()
